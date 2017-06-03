7dd7b0e845856ff384afdfc35141fd5329c550ae
lau <laurent@gre.fr>
2017-06-03 06:46:40 +0200

    premier niveau arbre OK

--- rallyman.py
+++ rallyman.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+def p(s):
+    print(s)
+
 class Tile:
     def __init__(self, type = 'straight', max_gears = []):
         self.max_gears = max_gears
@@ -14,26 +17,33 @@ class Tile:
                 return [-1]
             elif pos_on_tile == 1:
                 # 1 première partie du virage dérapé
-                return[0,2]
+                return [0,2]
             elif pos_on_tile == 2:
                 # 2 deuxième partie du virage dérapé
-                return[0,3]
+                return [0,3]
             elif pos_on_tile == 3:
                 # 3ème partie du virage dérapé: on sort après
-                return[-1]
+                return [-1]
         elif self.type == 'straight':
             if pos_on_tile == -1:
                 return [0]
             elif pos_on_tile == 0:
-                return[-1]  
+                return [-1]  
+        else:
+            return [-5]
     def get_max_gear(self, pos_on_tile):
-        if type == 'straight':
+        if self.type == 'straight':
             return 10
-        elif type == 'turn':
+        elif self.type == 'turn':
             if pos_on_tile == 0: #corde
                 return self.max_gears[0]
             else:  #dérapage
                 return self.max_gears[1]
+        else:
+            p("Troubles w/h max_gear")
+            return -50
+    def inspect(self):
+        print(self.type + ';' + str(self.max_gears))
                 
         
 class Road:
@@ -44,25 +54,29 @@ class Road:
         self.tiles.append(tile)
     
     def what_next(self, tile, pos_on_tile):
-        next_pos = self.tiles[tile].next_pos_on_tile(pos_on_tile)
-        if next_pos == [-1]: # next tile
+        nxt = self.tiles[tile].next_pos_on_tile(pos_on_tile)
+        if nxt == [-1]: # next tile
             pot_l = self.tiles[tile+1].next_pos_on_tile(-1)
             ret = []
             for pot in pot_l:
                 ret.append([1,pot])
             return ret
         else: #same tile
-            pot_l = self.tiles[tile].next_pos_on_tile(pos_on_tile)
             ret = []
-            for pot in pot_l:
+            for pot in nxt:
                 ret.append([0,pot])
             return ret
+    
+    def inspect(self):
+        print ('Road length:' + str(len(self.tiles)))
+        for tile in self.tiles:
+            tile.inspect()
             
             
         
 class State:
     def __init__(self,road):
-        self.road = Road()
+        self.road = road
         self.move = 0
         self.time = 0
         self.tile_position = 0
@@ -84,7 +98,10 @@ class State:
     def next_state(self, dice, move):
         # check if dice is valid
         tile, pot = move
-        max_auth = self.road.tiles[tile_position+tile].get_max_gear(pot)
+        p(tile)
+        p(pot)
+        max_auth = self.road.tiles[self.tile_position+tile].get_max_gear(pot)
+        p(max_auth)
         if dice > max_auth:
             return None
         
@@ -99,10 +116,21 @@ class State:
             ns.actual_speed= self.actual_speed
         else:
             ns.gaz = self.gaz
-            ns.dices = self.dices.remove(dice)            
+            ns.dices = self.dices    
+            ns.dices.remove(dice)
             ns.actual_speed = dice
         return ns
-            
+    
+    def inspect(self):
+        p("Inspect state:")
+        p(self.move)
+        p(self.time) 
+        p(self.tile_position) 
+        p(self.pos_on_tile)
+        p(self.time) 
+        p(self.dices) 
+        p(self.gaz) 
+        p(self.actual_speed)
 
 class Move_tree:
     def __init__(self, road):
@@ -113,24 +141,41 @@ class Move_tree:
     
     def find_successors(self):
         auth_dices = self.state.auth_dices()
-        print(auth_dices)
-        move = self.state.road.what_next(self.state.tile_position,
+        moves = self.state.road.what_next(self.state.tile_position,
             self.state.pos_on_tile)
-        print(move)
+        for d in auth_dices:
+            for m in moves:
+                n = self.state.next_state(d,m)
+                self.successors.append(n)
+        return self.successors
 
             
-            
-
-
 class Player:
     def __init__(self, road):
         self.state = State(road)
         
 
 
-first_road = Road()
+fr = Road()
+Straight = Tile('straight')
+Turn2 = Tile('turn', [2,3])
+fr.append(Straight)
+fr.append(Straight)
+fr.append(Straight)
+fr.append(Straight)
+fr.append(Turn2)
+fr.append(Straight)
+fr.append(Straight)
+fr.append(Turn2)
+fr.append(Straight)
+fr.append(Straight)
+fr.append(Straight)
+fr.inspect()
 
-m = Move_tree(first_road)
-m.find_successors()
+
+m = Move_tree(fr)
+suc = m.find_successors()
+for s in suc:
+    s.inspect()
 
 
